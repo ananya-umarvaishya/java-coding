c62b984b4262a9bab278fcaaea5792c90d3c7bb7
Ananya <ananyamsgupta123@gmail.com>
2026-08-22 23:06:28 +0530

    Added new program

---
+++ patterns/ques17.java
@@ -0,0 +1,11 @@
+public class ques17 {
+    public static void main(String args[]){
+        for(int i = 4; i >= 1; i--){
+            for(int j = 0; j < i; j++){
+                System.out.print((char)('a'+j));
+            }
+            System.out.println();
+        }
+    }
+    
+}
