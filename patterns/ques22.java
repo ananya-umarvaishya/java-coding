7dc857de3b6796eafa584eab24e1c0d8e8a391f0
Ananya <ananyamsgupta123@gmail.com>
2026-08-26 00:07:10 +0530

    Added new program

---
+++ patterns/ques22.java
@@ -0,0 +1,15 @@
+public class ques22 {
+    public static void main(String args[]){
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j<= i; j++){
+                if( i == j || j == i-2){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
