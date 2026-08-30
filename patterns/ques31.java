833b7f1ab26cfdfad48f0899bad8a432b6308ecf
Ananya <ananyamsgupta123@gmail.com>
2026-08-30 23:51:46 +0530

    Added new programs

--- patterns/ques31.java
+++ patterns/ques31.java
@@ -1,6 +1,21 @@
 public class ques31 {
     public static void main(String args[]){
-        
+        int n = 5;
+
+for (int i = 1; i <= n; i++) {
+
+    // print -
+    for (int j = 1; j < i; j++) {
+        System.out.print("- ");
+    }
+
+    // print X
+    for (int j = i; j <= n; j++) {
+        System.out.print("X ");
+    }
+
+    System.out.println();
+}
     }
     
 }
