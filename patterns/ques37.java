e12936bbeb65fa1dd70cedcfb79d1d077be4967a
Ananya <ananyamsgupta123@gmail.com>
2026-09-02 23:51:44 +0530

    Added new program

---
+++ patterns/ques37.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class ques37 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (i < j)
+                    System.out.print(i + " ");
+                else
+                    System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
